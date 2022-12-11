78a44926f267eb9e5ec6c4afc4d7f9a5ecfca4c3
Agil Tandiera <60023592+agiltandiera@users.noreply.github.com>
2022-12-11 16:12:01 +0800

    create layout toolbar and navigate signin page to signup page

--- app/src/main/java/com/tandiera/project/foodmarket/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/tandiera/project/foodmarket/ui/auth/AuthActivity.kt
@@ -2,11 +2,25 @@ package com.tandiera.project.foodmarket.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.tandiera.project.foodmarket.R
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        val pageRequest = intent.getIntExtra("page_request", 0)
+        if(pageRequest == 2) {
+            val navOptions = NavOptions.Builder()
+                .setPopUpTo(R.id.fragmentSignIn, true)
+                .build()
+
+            Navigation.findNavController(findViewById(R.id.authHostFragment))
+                .navigate(R.id.action_signup, null, navOptions)
+        }
+
     }
 }
--- app/src/main/java/com/tandiera/project/foodmarket/ui/auth/signin/SignInFragment.kt
+++ app/src/main/java/com/tandiera/project/foodmarket/ui/auth/signin/SignInFragment.kt
@@ -1,20 +1,38 @@
 package com.tandiera.project.foodmarket.ui.auth.signin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tandiera.project.foodmarket.R
+import com.tandiera.project.foodmarket.databinding.FragmentSignInBinding
+import com.tandiera.project.foodmarket.ui.auth.AuthActivity
 
 class SignInFragment : Fragment() {
+
+    private var _binding: FragmentSignInBinding? = null
+    private val binding get() =_binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+        _binding = FragmentSignInBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        binding.btnSignInSignIn.setOnClickListener {
+            val signup = Intent(activity, AuthActivity::class.java)
+            signup.putExtra("page_request", 2)
+            startActivity(signup)
+        }
     }
 
 }
