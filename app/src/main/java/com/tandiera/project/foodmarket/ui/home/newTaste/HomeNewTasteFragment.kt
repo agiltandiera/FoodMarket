a9388dab86c1b0eb32c226c0d2a7ede0aac6be84
Agil Tandiera <60023592+agiltandiera@users.noreply.github.com>
2022-12-17 12:08:58 +0800

    implement list new taste on homefragment

--- app/src/main/java/com/tandiera/project/foodmarket/ui/home/newTaste/HomeNewTasteFragment.kt
+++ app/src/main/java/com/tandiera/project/foodmarket/ui/home/newTaste/HomeNewTasteFragment.kt
@@ -5,9 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tandiera.project.foodmarket.R
+import com.tandiera.project.foodmarket.databinding.FragmentHomeBinding
+import com.tandiera.project.foodmarket.databinding.FragmentHomeNewTasteBinding
+import com.tandiera.project.foodmarket.model.dummy.HomeModel
+import com.tandiera.project.foodmarket.model.dummy.HomeVerticalModel
+import com.tandiera.project.foodmarket.ui.home.HomeAdapter
 
-class HomeNewTasteFragment : Fragment() {
+class HomeNewTasteFragment : Fragment(), HomeNewTasteAdapter.ItemAdapterCallback {
+
+    private var _binding: FragmentHomeNewTasteBinding? = null
+    private val binding get() = _binding!!
+
+    private var foodList : ArrayList<HomeVerticalModel> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +28,27 @@ class HomeNewTasteFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home_new_taste, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        initDataDummy()
+        var adapter = HomeNewTasteAdapter(foodList.this)
+        var layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(activity)
+        binding.rvListHomeNewTaste.layoutManager = layoutManager
+        binding.rvListHomeNewTaste.adapter = adapter
+    }
+
+    fun initDataDummy() {
+        foodList = ArrayList()
+        foodList.add(HomeVerticalModel("Cherry Healthy", "10000", "",5f))
+        foodList.add(HomeVerticalModel("Burger Tamayo", "10000", "",4f))
+        foodList.add(HomeVerticalModel("Alpokat Kocok", "10000", "",4.8f))
+    }
+
+    override fun onClick(v: View, data: HomeVerticalModel) {
+        Toast.makeText(context,"Percobaan klik item" + data.title, Toast.LENGTH_LONG).show()
     }
 
 }
